a2a33c881cbb8faa3000becf187a8af422c81994
jarbasai <jarbasai@mailfence.com>
2022-03-25 23:57:35 +0000

    :tada: :tada:

--- neon_segmentation_plugin_nnsplit/__init__.py
+++ neon_segmentation_plugin_nnsplit/__init__.py
@@ -11,9 +11,21 @@ class NNSplitSegmenter(Segmenter):
 
     def segment(self, text):
         chunks = []
-        for split in self.splitter.split(super().segment(text))[0]:
-            # HACK - there is some bug in NNSplit, cant get the strings because iterating casts to list again...
-            stringified = str(split)[1:-1].strip()
-            stringified = [s.split("', whitespace=")[0] for s in stringified.split("Token(text='")]
-            chunks.append(" ".join(stringified).strip())
+        splitted = self.splitter.split(super().segment(text))
+        for splits in splitted:
+            for split in splits:
+                # HACK - there is some bug in NNSplit, cant get the strings because iterating casts to list again...
+                stringified = str(split).replace("[", "").replace("]", "").strip()
+                stringified = [s[1:].split(", whitespace=")[0][:-1] for s in stringified.split("Token(text=")]
+                stringified = " ".join(stringified).strip()\
+                              .replace(" n't", "n't")\
+                              .replace(" , ", ", ")\
+                              .replace(" . ", ". ")\
+                              .replace(" ...", "...") \
+                              .replace(" !", "!") \
+                              .replace(" ?", "?")
+                if stringified.endswith(" ."):
+                    stringified = stringified[:-2] + "."
+
+                chunks.append(stringified)
         return chunks or [text]
--- test/test_split.py
+++ test/test_split.py
@@ -8,8 +8,24 @@ class TestNNSplitSegmenter(unittest.TestCase):
     def test_segment(self):
         solver = NNSplitSegmenter()
         self.assertEqual(solver.segment(
-            "This is a test This is another test."),
+            "This is a test This is another test"),
             ["This is a test", "This is another test"])
         #self.assertEqual(solver.segment(
         #    "I am Batman I live in gotham"),
         #    ["I am Batman", "I live in gotham"])
+
+        # test internal base class heuristic still works
+        test_sent = "Mr. Smith bought cheapsite.com for 1.5 million " \
+                    "dollars, i.e. he paid a lot for it. Did he mind? Adam " \
+                    "Jones Jr. thinks he didn't. In any case, this isn't true..." \
+                    " Well, with a probability of .9 it isn't."
+        self.assertEqual(
+            solver.segment(test_sent),
+            [
+                'Mr. Smith bought cheapsite.com for 1.5 million dollars, i.e. he paid a lot for it.',
+                'Did he mind?',
+                "Adam Jones Jr. thinks he didn't.",
+                "In any case, this isn't true...",
+                "Well, with a probability of .9 it isn't."
+            ]
+        )
